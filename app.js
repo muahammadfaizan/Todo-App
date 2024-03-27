2cf3a6854aeadd52fbbffa9d8bfea5e7143c1b56
Muhammad Faizan <fiazansuri855@gmail.com>
2024-03-27 07:03:14 +0500

    JS Todo app assginment

--- app.js
+++ app.js
@@ -1,171 +1,33 @@
-// console.log('Todo App');
-
-
-
-
-
-
-// const fruits = ['apple' , 'mango' , 'banana' , 'orange' , 'strwaberry'];
-
-
-
-// for(let i = 0 ; i < fruits.length ; i++){
-//     console.log(fruits[i])
-// }
-
-
-
-
-
-
-
-
-
-// const fruits = ['apple' , 'mango' , 'banana' , 'orange' , 'strwaberry'];
-
-// const ol = document.querySelector('ol');
-
-// for(let i = 0 ; i < fruits.length ; i++){
-//     console.log(fruits[i])
-
-//     ol.innerHTML += `<li>${fruits[i]}</li>`
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// console.log('hello loops');
 
 
 const arr = []
 const input = document.querySelector('#todoapp');
-
-const ol = document.querySelector('ol');
-
-
-
-// function renderTodo(){
-
-//     ol.innerHTML = ''
-
-//     for(let i = 0 ; i < arr.length ; i++){
-//         console.log(arr[i])
-
-//         ol.innerHTML += `<li>
-//         ${arr[i]}
-//         <button onclick="deleteTodo()">Delete</button>
-//         <button onclick="editTodo()">edit</button></li>`
-//     }
-// }
-
-
-
-
-
-
-
-
-
-// function getTodo(){
-
-//     arr.push(input.value)
-//     renderTodo()
-//     console.log(arr)
-//     input.value = ''
-// }
-
-
-
-
-
-
-// function deleteTodo(index){
-
-//     arr.splice(index , 1)
-//     renderTodo()
-// }
-
-
-
-// function editTodo(index){
-   
-//     const updatedValue = prompt('enter your updated value');
-//     arr.splice(index , 1 , updatedValue);
-
-//     renderTodo()
-
-// }
-
-
-
-function addTodo(){
-
-    ol.innerHTML = ''
-
-    for(let i = 0 ; i < arr.length ; i++){
-        console.log(arr[i])
-        ol.innerHTML += `
-        <li>${arr[i]}
-        <button onclick="deleteTodo()"><i class="fa-solid fa-trash"></i></button>
-        <button onclick="deleteTodo()"><i class="fa-regular fa-pen-to-square"></i></button> </li>`
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
+const ol = document.querySelector('ol')
 
 
 function getTodo(){
 
-    arr.push(input.value);
-    addTodo()
-    input.value = '' ;
-
-
+    arr.push(input.value)
+    renderTodo()
+    input.value = ''
 }
 
 
 
-
-
-
-function deleteTodo(index){
-
+function deleteTodo(index) {
+    
     arr.splice(index , 1);
-    addTodo()
-    input.value = '';
+    renderTodo()
+
 }
-
-
-
-
-
 
 
 function editTodo(index){
 
     const updatedValue = prompt('enter your update value');
-    
-    arr.splice(index , 1 , updatedValue) ;
-    addTodo()
-    input.value = '';
+    arr.splice(index , 1 , updatedValue)
+    renderTodo()
 }
 
 
@@ -177,19 +39,20 @@ function editTodo(index){
 
 
 
+function renderTodo(){
+    
+    ol.innerHTML = ''
 
+    for(let i = 0 ; i < arr.length ; i++){
+        console.log(arr[i])
+        ol.innerHTML += 
+       `<li class="text-center border">
+        ${arr[i]}
+        <button class = "btn btn-primary" onclick="deleteTodo()"><i class="fa-solid fa-trash"></i></button>  
 
-
-
-
-
-
-
-
-
-
-
-
+        <button class = "btn btn-primary" onclick="editTodo()"><i class="fa-regular fa-pen-to-square"></i></button> <li>`
+    }
+} 
 
 
 
